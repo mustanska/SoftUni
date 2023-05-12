8fede4de24b4bdbf832b76a29af80474a67650b9
mustanska <122823838+mustanska@users.noreply.github.com>
2023-05-13 01:14:02 +0300

    Update array_modifier.py
    Add the code in array_modifier.py

--- Fundamentals/Exams/Middle/array_modifier.py
+++ Fundamentals/Exams/Middle/array_modifier.py
@@ -1,1 +1,28 @@
 
+int_array = [int(x) for x in input().split()]
+
+while True:
+    line = input()
+
+    if line == "end":
+        break
+
+    command_data = line.split()
+    command = command_data[0]
+
+    if command == "swap":
+        first_index = int(command_data[1])
+        second_index = int(command_data[2])
+
+        int_array[first_index], int_array[second_index] = int_array[second_index], int_array[first_index]
+
+    elif command == "multiply":
+        first_index = int(command_data[1])
+        second_index = int(command_data[2])
+
+        int_array[first_index] = int_array[first_index] * int_array[second_index]
+
+    elif command == "decrease":
+        int_array = [x - 1 for x in int_array]
+
+print(*int_array, sep=", ")
