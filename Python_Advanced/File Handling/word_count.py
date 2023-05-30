8b90d361850de9ef55bc8033d8d253c41bdde5a2
mustanska <122823838+mustanska@users.noreply.github.com>
2023-05-31 00:02:41 +0300

    Added solution for words count in file

---
+++ Python_Advanced/File Handling/word_count.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+words_file_path = os.path.join(root_path, "words.txt")
+text_file_path = os.path.join(root_path, "input.txt")
+result_file_path = os.path.join(root_path, "output.txt")
+
+word_count = {}
+
+with open(words_file_path, "r") as words_file:
+    words = words_file.read().lower().split()
+
+with open(text_file_path, "r") as text_file:
+    text = text_file.read().lower()
+
+for word in words:
+    pattern = r"[^a-z]" + re.escape(word) + r"[^a-z]"
+    matches = re.findall(pattern, text)
+
+    if matches:
+        word_count[word] = len(matches)
+
+result = ""
+for key, value in sorted(word_count.items(), key=lambda x: -x[1]):
+    result += f"{key} - {value}\n"
+
+with open(result_file_path, "w") as result_file:
+    result_file.write(result)
