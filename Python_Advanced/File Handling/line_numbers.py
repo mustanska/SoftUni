59a62ef2376ed8f91efc1b31df42674309e720ac
mustanska <122823838+mustanska@users.noreply.github.com>
2023-05-31 21:00:24 +0300

    Added line_numbers.py and change the file output.txt

---
+++ Python_Advanced/File Handling/line_numbers.py
@@ -0,0 +1,23 @@
+import os
+from string import punctuation
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(root_path, "text.txt")
+result_file_path = os.path.join(root_path, "output.txt")
+
+with open(file_path, "r") as file:
+    rows = file.readlines()
+
+with open(result_file_path, "w") as result:
+    for i in range(len(rows)):
+        current_row = rows[i]
+        letters = 0
+        punctuation_marks = 0
+
+        for character in current_row:
+            if character.isalpha():
+                letters += 1
+            elif character in punctuation:
+                punctuation_marks += 1
+
+        result.write(f"Line {i + 1}: {current_row[:-1]} ({letters})({punctuation_marks})\n")
