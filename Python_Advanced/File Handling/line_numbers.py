7aa59b87d08fb6e3e7be24018f6f8feb49bfef11
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-02 12:33:22 +0300

    Made some changes in line_numbers.py

--- Python_Advanced/File Handling/line_numbers.py
+++ Python_Advanced/File Handling/line_numbers.py
@@ -3,21 +3,22 @@ from string import punctuation
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(root_path, "Text Files", "text.txt")
-result_file_path = os.path.join(root_path, "Text Files", "output.txt")
 
 with open(file_path, "r") as file:
     rows = file.readlines()
 
-with open(result_file_path, "w") as result:
+with open("output.txt", "w") as result_file:
     for i in range(len(rows)):
-        current_row = rows[i]
+        row = rows[i]
         letters = 0
         punctuation_marks = 0
 
-        for character in current_row:
+        for character in row:
             if character.isalpha():
                 letters += 1
             elif character in punctuation:
                 punctuation_marks += 1
 
-        result.write(f"Line {i + 1}: {current_row[:-1]} ({letters})({punctuation_marks})\n")
+        result = f"Line {i + 1}: {row[:-1] if i < len(rows) - 1 else row} ({letters})({punctuation_marks})\n"
+        result_file.write(result)
+        
