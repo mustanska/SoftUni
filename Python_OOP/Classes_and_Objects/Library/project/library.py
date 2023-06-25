90d7a620191b7d98c4386caf6f47f2321574d69d
mustanska <122823838+mustanska@users.noreply.github.com>
2023-06-25 19:16:10 +0300

    Added a solution of Library from Classes and Objects Exercise

---
+++ Python_OOP/Classes_and_Objects/Library/project/library.py
@@ -0,0 +1,33 @@
+from typing import List, Dict
+
+from project.user import User
+
+
+class Library:
+    def __init__(self):
+        self.user_records: List[User] = []
+        self.books_available: Dict[str, List[str]] = {}
+        self.rented_books: Dict[str, Dict[str, int]] = {}
+
+    def get_book(self,author: str, book_name: str, days_to_return: int, user: User) -> str:
+        if book_name in self.books_available[author]:
+            user.books.append(book_name)
+            self.books_available[author].remove(book_name)
+
+            if user.username not in self.rented_books:
+                self.rented_books[user.username] = {}
+            self.rented_books[user.username][book_name] = days_to_return
+
+            return f"{book_name} successfully rented for the next {days_to_return} days!"
+
+        days_left = self.rented_books[user.username][book_name]
+        return f'The book "{book_name}" is already rented and will be available in {days_left} days!'
+
+    def return_book(self, author: str, book_name: str, user: User) -> str or None:
+        if book_name not in user.books:
+            return f"{user.username} doesn't have this book in his/her records!"
+
+        user.books.remove(book_name)
+        del self.rented_books[user.username][book_name]
+        self.books_available[author].append(book_name)
+
