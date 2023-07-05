4b08c1b6ef94ea57f54445c93dd4fbac85b1640d
mustanska <122823838+mustanska@users.noreply.github.com>
2023-07-05 18:26:36 +0300

    Added a solution of Gym from Static and Class Methods Exercise

---
+++ Python_OOP/Static_and_Class_Methods/Gym/project/gym.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from project.customer import Customer
+from project.equipment import Equipment
+from project.exercise_plan import ExercisePlan
+from project.subscription import Subscription
+from project.trainer import Trainer
+
+
+class Gym:
+    def __init__(self):
+        self.customers: List[Customer] = []
+        self.trainers: List[Trainer] = []
+        self.equipment: List[Equipment] = []
+        self.plans: List[ExercisePlan] = []
+        self.subscriptions: List[Subscription] = []
+
+    def add_customer(self, customer: Customer) -> None:
+        if customer not in self.customers:
+            self.customers.append(customer)
+
+    def add_trainer(self, trainer: Trainer) -> None:
+        if trainer not in self.trainers:
+            self.trainers.append(trainer)
+
+    def add_equipment(self, equipment: Equipment) -> None:
+        if equipment not in self.equipment:
+            self.equipment.append(equipment)
+
+    def add_plan(self, plan: ExercisePlan) -> None:
+        if plan not in self.plans:
+            self.plans.append(plan)
+
+    def add_subscription(self, subscription: Subscription) -> None:
+        if subscription not in self.subscriptions:
+            self.subscriptions.append(subscription)
+
+    def subscription_info(self, subscription_id: int) -> str:
+        subscription = [s for s in self.subscriptions if s.id == subscription_id][0]
+        customer = [c for c in self.customers if subscription.customer_id == c.id][0]
+        trainer = [t for t in self.trainers if subscription.trainer_id == t.id][0]
+        plan = [e for e in self.plans if subscription.exercise_id == e.id][0]
+        equipment = [e for e in self.equipment if plan.equipment_id == e.id][0]
+
+        return f"{subscription}\n{customer}\n{trainer}\n{equipment}\n{plan}"
