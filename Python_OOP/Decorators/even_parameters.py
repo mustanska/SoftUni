8614ccddd540fd21d1a27e490761700747f1b945
mustanska <122823838+mustanska@users.noreply.github.com>
2023-07-19 20:47:54 +0300

    Added first six solutions from Decorators Exercise

---
+++ Python_OOP/Decorators/even_parameters.py
@@ -0,0 +1,34 @@
+def even_parameters(function):
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if isinstance(arg, int):
+                if arg % 2 == 0 and isinstance(arg, int):
+                    continue
+
+            return "Please use only even numbers!"
+
+        return function(*args, *kwargs)
+
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+
+    return result
+
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
