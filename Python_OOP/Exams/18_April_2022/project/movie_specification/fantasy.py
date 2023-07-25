26459db461106efebd689494dd324dc4a3e662f2
mustanska <122823838+mustanska@users.noreply.github.com>
2023-07-25 18:40:51 +0300

    Added the solution of first two problems from Exam 18 April 2022

---
+++ Python_OOP/Exams/18_April_2022/project/movie_specification/fantasy.py
@@ -0,0 +1,19 @@
+from project.movie_specification.movie import Movie
+
+
+class Fantasy(Movie):
+    def __init__(self, title: str, year: int, owner: object, age_restriction: int = 6):
+        super().__init__(title, year, owner, age_restriction)
+
+    @property
+    def age_restriction(self):
+        return self.__age_restriction
+
+    @age_restriction.setter
+    def age_restriction(self, value):
+        if value < 6:
+            raise ValueError("Fantasy movies must be restricted for audience under 6 years!")
+
+        self.__age_restriction = value
+
+
