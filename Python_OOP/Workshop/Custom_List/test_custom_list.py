c3c56e2a54e3494dbfb9d4fdbae8e2ee0abc4ef4
mustanska <122823838+mustanska@users.noreply.github.com>
2023-08-18 18:03:49 +0300

    Added new tests for Custom List

--- Python_OOP/Workshop/Custom_List/test_custom_list.py
+++ Python_OOP/Workshop/Custom_List/test_custom_list.py
@@ -97,6 +97,79 @@ class TestCustomList(TestCase):
         self.assertEqual(excepted, self.custom_list.list)
         self.assertEqual(8, result)
 
+    def test_get_element_by_index(self):
+        with self.assertRaises(IndexError) as ie:
+            self.custom_list.get(10)
+        self.assertEqual("Index is out of range!", str(ie.exception))
+
+        result = self.custom_list.get(1)
+        self.assertEqual("hello", result)
+
+    def test_extend_list_with_non_iterable_element_raises(self):
+        with self.assertRaises(ValueError) as ve:
+            self.custom_list.extend(1.5)
+
+        self.assertEqual("This object is not iterable!", str(ve.exception))
+
+        result = self.custom_list.extend(["hi", 11, "bye"])
+        excepted = [1, "hello", 3, [2, 3, 4], 5.5, 8, (1, "me"), 3, -2, "hi", 11, "bye"]
+        self.assertEqual(excepted, result)
+
+    def test_insert_value_on_index(self):
+        with self.assertRaises(IndexError) as ie:
+            self.custom_list.insert(-10, ["hi", 11, "bye"])
+        self.assertEqual("Index is out of range!", str(ie.exception))
+
+        result = self.custom_list.insert(-5, ["hi", 11, "bye"])
+        excepted = [1, "hello", 3, [2, 3, 4], ["hi", 11, "bye"], 5.5, 8, (1, "me"), 3, -2]
+        self.assertEqual(excepted, result)
+
+    def test_pop_last_element_from_list(self):
+        result = self.custom_list.pop()
+        self.assertEqual(-2, result)
+
+    def test_clear_list(self):
+        self.custom_list.clear()
+        self.assertEqual([], self.custom_list.list)
+
+    def test_index_of_value(self):
+        with self.assertRaises(ValueError) as ve:
+            self.custom_list.index(0)
+
+        self.assertEqual("The value does not exist in the list.", str(ve.exception))
+
+        result = self.custom_list.index(5.5)
+        self.assertEqual(4, result)
+
+    def test_count_of_value(self):
+        result = self.custom_list.count([1, 2])
+        self.assertEqual(0, result)
+
+        result = self.custom_list.count(3)
+        self.assertEqual(2, result)
+
+    def test_reverse_list_method(self):
+        result = self.custom_list.reverse()
+        expected = [-2, 3, (1, "me"), 8, 5.5, [2, 3, 4], 3, "hello", 1]
+        self.assertEqual(expected, result)
+
+    def test_copy_list_method(self):
+        result = self.custom_list.copy()
+        expected = [1, "hello", 3, [2, 3, 4], 5.5, 8, (1, "me"), 3, -2]
+        self.assertEqual(expected, result)
+        self.assertNotEqual(id(result), id(self.custom_list.list))
+
+    def test_size_list_method(self):
+        result = self.custom_list.size()
+        self.assertEqual(9, result)
+
+    def test_add_first_element_in_list(self):
+        self.custom_list.add_first(0)
+        expected = [0, 1, "hello", 3, [2, 3, 4], 5.5, 8, (1, "me"), 3, -2]
+        self.assertEqual(expected, self.custom_list.list)
+        self.assertEqual(0, self.custom_list.list[0])
+
+    
 
 if __name__ == "__main__":
     main()
